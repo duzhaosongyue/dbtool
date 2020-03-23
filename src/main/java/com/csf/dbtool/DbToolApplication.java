ae91b2aa6609534feccfedf4b3054d9942a03aad
fuping <1322484007>
2020-03-23 12:03:34 +0800

    fix:初始化项目

--- src/main/java/com/csf/dbtool/DbToolApplication.java
+++ src/main/java/com/csf/dbtool/DbToolApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DbToolApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DbToolApplication.class, args);
     }
